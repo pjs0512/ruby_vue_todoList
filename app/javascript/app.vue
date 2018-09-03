ea6f6d810da8d5ace59e391978267d9db131f39e
EC2 Default User <ec2-user@ip-172-31-8-23.ap-southeast-1.compute.internal>
2018-09-03 03:32:11 +0000

    todoList

---
+++ app/javascript/app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="tir">
+    <p>{{ message }}</p>
+    <p>{{ test }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      // この文字列が画面に表示されている
+      test : "느금마 올라프,",
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
